62d9ffa420fc0570485871225d42571300ba7c42
PJAheibam <prosenjitsingha28@gmail.com>
2022-10-27 23:42:41 +0600

    pic changes

--- src/pages/courses/Course.jsx
+++ src/pages/courses/Course.jsx
@@ -86,7 +86,7 @@ const Course = () => {
             component="img"
             width="100%"
             height={300}
-            image="https://source.unsplash.com/random/300×300"
+            image="https://source.unsplash.com/random/300×300/?coding"
           />
         </Grid>
       </Grid>
